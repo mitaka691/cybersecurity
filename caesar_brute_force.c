ecf14458d48813d9cf5f51502ba58361c3c206fe
mitaka691 <141424845+mitaka691@users.noreply.github.com>
2026-03-10 23:47:42 +0200

    Add files via upload

---
+++ caesar_brute_force.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+void caesar_decrypt(char *input, char *output, int key) {
+    int len = strlen(input);
+
+    for (int i = 0; i < len; i++) {
+        char c = input[i];
+
+        if (isupper(c)) {
+            output[i] = (c - 'A' - key + 26) % 26 + 'A';
+        } 
+        else {
+            output[i] = c; // Intervali i drugi simvoli
+        }
+    }
+
+    output[len] = '\0';
+}
+
+void bruteforce_attack(char *cipher) {
+    char buffer[256];
+
+    printf("--- BRUTE FORCE REPORT ---\n");
+
+    for (int k = 1; k < 26; k++) {
+        caesar_decrypt(cipher, buffer, k);
+        printf("[Key %02d]: %s\n", k, buffer);
+    }
+}
+
+int main() {
+    char cipher[] = "YVIBZM VM DOT ZVM ADMO VN D OZVXC D TJP V WPO";
+
+    bruteforce_attack(cipher);
+
+    return EXIT_SUCCESS;
+}
